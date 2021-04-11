adfd84c44401eabcaab24bf6d1ef50477eac09d3
Douglas Sartori <doug@sartori.ca>
2021-04-11 16:20:44 -0400

    add ini file add Expenses source document load data from BOC API load data from Expenses doc manipulate and join tables

--- ETLDemo.py
+++ ETLDemo.py
@@ -3,6 +3,55 @@ import os
 import petl
 import sys
 import pymssql
-import logging as log
 import configparser
+import requests
+import datetime
+import json
+import decimal
+
+# get data from configuration file
+config = configparser.ConfigParser()
+config.read('ETLDemo.ini')
+
+# read settings from configuration file
+startDate = config['CONFIG']['startDate']
+url = config['CONFIG']['url']
+
+# request data from URL
+BOCResponse = requests.get(url+startDate)
+
+# initialize list of lists for data storage
+BOCDates = []
+BOCRates = []
+
+# check response status and process BOC JSON object
+if (BOCResponse.status_code == 200):
+    BOCRaw = json.loads(BOCResponse.text)
+
+    # extract observation data into column arrays
+    for row in BOCRaw['observations']:
+        BOCDates.append(datetime.datetime.strptime(row['d'],'%Y-%m-%d'))
+        BOCRates.append(decimal.Decimal(row['FXUSDCAD']['v']))
+
+# create petl table from column arrays and rename the columns
+exchangeRates = petl.fromcolumns([BOCDates,BOCRates])
+exchangeRates = petl.rename(exchangeRates,{'f0': 'date','f1':'rate'})
+print (exchangeRates)
+
+# load expense document
+expenses = petl.io.xlsx.fromxlsx('Expenses.xlsx',sheet='Github')
+
+# join tables
+expenses = petl.leftjoin(exchangeRates,expenses,key='date')
+
+# fill down missing values
+expenses = petl.filldown(expenses,'rate')
+
+# remove dates with no expenses
+expenses = petl.select(expenses,lambda rec: rec.US != None)
+
+# add CDN column
+expenses = petl.addfield(expenses,'CDN', lambda rec: rec.US * rec.rate)
+
+print (expenses)
 
